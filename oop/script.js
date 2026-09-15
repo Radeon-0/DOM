915009c5c0ddf355ae5110944c8a4825af16a1c2
Radion0 <radion.glushkov@mail.ru>
2026-09-15 16:46:27 +0700

    Дз ооп

--- oop/script.js
+++ oop/script.js
@@ -50,3 +50,66 @@ const block2 = new DomElement(
 
 block.createElement();
 block2.createElement();
+
+
+// class Person {
+//     constructor(name,age){
+//         this.name = name;
+//         this.age = age;
+//     }
+//     sayHello(){
+//         console.log(`Привет! меня зовут ${this.name}`);
+//     }
+// }
+
+// class FrontEndDev extends Person {
+//     constructor(name,age,skills = []){
+//         super(name,age);
+//         this._skills = skills;
+//     }
+//     get skills(){
+//         return this._skills;
+//     }
+
+//     set skills(str){
+//         this.skills.push(str);
+//     }
+
+// }
+
+// const dev = new FrontEndDev('vlad',23);
+// dev.skills = 'text';
+// dev.skills = 'text2';
+// dev.skills = 'text3';
+// console.log(dev.skills);
+
+
+// const user = {
+//     name:'Alex',
+//     age:33,
+//     isAuth:false,
+//     project:{
+//         first: 'first',
+//         secound: 'secound'
+//     }
+// };
+
+// const {name,age,isAuth,project,project:{first,secound}} = user;
+
+// console.log(project);
+// console.log(first);
+// console.log(secound);
+
+// const names = ['artyom','Max','Vasya',['Petya','Alex']];
+
+// const [art,max,vasya,[petya,alex]] = names;
+
+// console.log(art);
+// console.log(petya);
+// console.log(alex);
+
+// const logger = ({first,second,third})=>{
+//     console.log(second);
+// };
+
+// logger({first:'I',second:'Js',third:"2"})
